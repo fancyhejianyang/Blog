82b919e0b1618191d998a930fc35cc365b6462c7
fancyhejianyang <18651436384@163.com>
2016-09-27 00:11:05 +0800

    mongodb相关包

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -11,4 +11,7 @@ module.exports = function(app){
   app.get('/',function(req,res){
     res.render('index',{title:'主页'});
   });
+  app.get('/login',function(req,res){
+    res.render('login',{title:'登录'});
+  })
 };
